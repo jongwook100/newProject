9f51efcf24035d0e7f43e466ca00b4dd16af74ac
jongwook100 <55087000+jongwook100@users.noreply.github.com>
2019-10-02 23:09:14 +0900

    version2
    version2

--- app/src/main/java/org/techtown/newproject/blackrice.java
+++ app/src/main/java/org/techtown/newproject/blackrice.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class riceActivity extends AppCompatActivity {
+public class blackrice extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rice);
+        setContentView(R.layout.activity_blackrice);
     }
 }
--- app/src/main/java/org/techtown/newproject/foodlist.java
+++ app/src/main/java/org/techtown/newproject/foodlist.java
@@ -1,8 +1,12 @@
 package org.techtown.newproject;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,18 +15,58 @@ import org.techtown.newproject.R;
 import java.util.LinkedList;
 
 public class foodlist extends AppCompatActivity {
-
+    Button button11;
+    LinearLayout layout;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodlist);
+
+        int count = 0;
         Button a = (Button)findViewById(R.id.button1);
         Button b = (Button)findViewById(R.id.button2);
+        Button c = (Button)findViewById(R.id.button3);
+        Button d = (Button)findViewById(R.id.button4);
+        Button e = (Button)findViewById(R.id.button5);
+        Button f = (Button)findViewById(R.id.button6);
+        Button g = (Button)findViewById(R.id.button7);
+        Button h = (Button)findViewById(R.id.button8);
+        Button i = (Button)findViewById(R.id.button9);
+        Button j = (Button)findViewById(R.id.button10);
 
+        button11= (Button)findViewById(R.id.button11);
+        layout = (LinearLayout)findViewById(R.id.layout);
+        context = this;
+        //버튼을 링크드리스트로 구현하기
         LinkedList<Button> bt = new LinkedList<Button>();
         bt.add(a);
         bt.add(b);
-        bt.get(0).setText("김치찌개");
-        bt.get(1).setText("된장찌개");
+        bt.add(c);
+        bt.add(d);
+        bt.add(e);
+        bt.add(f);
+        bt.add(g);
+        bt.add(h);
+        bt.add(i);
+        bt.add(j);
+        //흑미밥 레시피 넘어가는 코드
+        a.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(), blackrice.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+       button11.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               Button btn = new Button(context);
+               btn.setText("버튼"+String.valueOf(1));
+               layout.addView(btn);
+           }
+       });
+
     }
 }
