c894c090b58ba070590c9fb0aeccd710f42f2c89
N0mercy00 <dkdhehesp@naver.com>
2019-10-14 17:55:13 +0900

    Feeling layout ver.1.2

---
+++ app/src/main/java/org/techtown/newproject/SelectActivity2.java
@@ -0,0 +1,60 @@
+
+package org.techtown.newproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class SelectActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select2);
+
+        //
+        //activity_select2 에서 버튼 누르면 activity_select3로 이동하는 버튼 세개
+        Button move_two_one=findViewById(R.id.selectButton_Two_One);
+        move_two_one.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(), SelectActivity3.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        Button move_two_two=findViewById(R.id.selectButton_Two_Two);
+        move_two_two.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(), SelectActivity3.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        Button move_two_thr=findViewById(R.id.selectButton_Two_Thr);
+        move_two_thr.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(), SelectActivity3.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+
+
+
+    }
+
+}
