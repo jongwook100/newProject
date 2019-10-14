550e66f8f57f870b66ba3dd0c0a9e300d9646fed
N0mercy00 <dkdhehesp@naver.com>
2019-10-14 17:52:52 +0900

    Feeling layout ver.1.2

--- app/src/main/java/org/techtown/newproject/MainActivity.java
+++ app/src/main/java/org/techtown/newproject/MainActivity.java
@@ -38,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+
+
         //
 
         Button infoButton=findViewById(R.id.informbutton);
--- app/src/main/java/org/techtown/newproject/SelectActivity.java
+++ app/src/main/java/org/techtown/newproject/SelectActivity.java
@@ -1,6 +1,7 @@
 
 package org.techtown.newproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class SelectActivity extends AppCompatActivity {
 
@@ -17,6 +19,38 @@ public class SelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+        //activity_select 에서 버튼 누르면 activity_select2로 이동하는 버튼 세개
+        Button move_one_one=findViewById(R.id.selectButton_One_One);
+        move_one_one.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(), SelectActivity2.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        Button move_one_two=findViewById(R.id.selectButton_One_Two);
+        move_one_two.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(), SelectActivity2.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        Button move_one_thr=findViewById(R.id.selectButton_One_Thr);
+        move_one_thr.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(), SelectActivity2.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
     }
 
 }
