4b9334b5cea8063321cf15fee021dc78f1494ef5
N0mercy00 <dkdhehesp@naver.com>
2019-10-14 17:59:54 +0900

    Feeling layout ver.1.2

--- app/src/main/java/org/techtown/newproject/FinalActivity.java
+++ app/src/main/java/org/techtown/newproject/FinalActivity.java
@@ -21,13 +21,16 @@ public class FinalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_final);
         //레시피 보러가는 버튼
 
+        //
+
+
 
         //최종 메뉴 추천후 돌아가기 버튼
         Button move_thr_one=findViewById(R.id.selectButton_Fin_Two);
         move_thr_one.setOnClickListener(
                 new Button.OnClickListener(){
                     public void onClick(View view){
-                        Intent intent=new Intent(getApplicationContext(), SelectActivity.class);
+                        Intent intent=new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(intent);
                     }
                 }
