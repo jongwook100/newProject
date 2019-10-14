ba82b1b46a31df3e8b2655663cfc6d9be57d70d3
N0mercy00 <dkdhehesp@naver.com>
2019-10-14 17:57:59 +0900

    Feeling layout ver.1.2

--- app/src/main/java/org/techtown/newproject/FinalActivity.java
+++ app/src/main/java/org/techtown/newproject/FinalActivity.java
@@ -1,6 +1,7 @@
 
 package org.techtown.newproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class FinalActivity extends AppCompatActivity {
 
@@ -17,8 +19,22 @@ public class FinalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+        //레시피 보러가는 버튼
+
+
+        //최종 메뉴 추천후 돌아가기 버튼
+        Button move_thr_one=findViewById(R.id.selectButton_Fin_Two);
+        move_thr_one.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(), SelectActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
     }
-    //
+
 
 
 }
